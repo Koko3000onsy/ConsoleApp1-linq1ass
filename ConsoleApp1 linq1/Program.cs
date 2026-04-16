e41f8e302ae5228d57da949aec3abaa871f9e448
Koko3000onsy <onsykoko79@gmail.com>
2026-04-16 12:37:51 +0200

    Add project files.

---
+++ ConsoleApp1 linq1/Program.cs
@@ -0,0 +1,11 @@
+﻿using static ConsoleApp1_linq1.DataSource.Source;
+namespace ConsoleApp1_linq1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
